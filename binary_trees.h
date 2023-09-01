b9d7de9efaa55977fb77dd281cdf5779806750d6
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-09-01 23:52:30 +0000

    Task 5

--- binary_trees.h
+++ binary_trees.h
@@ -83,6 +83,7 @@ heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
 
 /* UTILITIES */
 void binary_tree_print(const binary_tree_t *);
