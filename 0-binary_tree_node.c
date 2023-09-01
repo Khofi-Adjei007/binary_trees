a6b5f5fdec1fd4d29505f97e6c1dea056fc90e3f
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-09-01 16:12:49 -0700

    Test 0 Cleared

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,29 +2,30 @@
 #include <stdlib.h>       /* Include the standard library for malloc */
 
 /**
- * binary_tree_node - creates a binary tree node
- * @parent: pointer to the parent node of the new node to create
- * @value: the integer value to store in the new node
- *
- * Description:
- * This function creates a new binary tree node with the specified parent
- * node and integer value.
- *
- * Return: pointer to the newly created binary tree node
- */
+* binary_tree_node - creates a binary tree node
+* @parent: pointer to the parent node of the new node to create
+* @value: the integer value to store in the new node
+*
+* Description:
+* This function creates a new binary tree node with the specified parent
+* node and integer value.
+*
+* Return: pointer to the newly created binary tree node
+*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node; /* Declare a pointer to a binary tree node */
+binary_tree_t *new_node; /* Declare a pointer to a binary tree node */
 
-    /* Allocate memory for the new node using malloc */
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL) /* Check if malloc was successful */
-        return (NULL);    /* Return NULL if memory allocation fails */
+/* Allocate memory for the new node using malloc */
+new_node = malloc(sizeof(binary_tree_t));
+if (new_node == NULL) /* Check if malloc was successful */
+return (NULL);    /* Return NULL if memory allocation fails */
 
-    /* Initialize the new node with the provided values */
-    new_node->parent = parent;               /* Set the parent pointer */
-    new_node->n = value;                     /* Set the value stored in the node */
-    new_node->left = new_node->right = NULL; /* Initialize left and right children */
+/* Initialize the new node with the provided values */
+new_node->parent = parent;               /* Set the parent pointer */
+new_node->n = value;                     /* Set the value stored in the node */
+new_node->left = new_node->right = NULL;
+/* Initialize left and right children */
 
-    return (new_node); /* Return a pointer to the newly created node */
+return (new_node); /* Return a pointer to the newly created node */
 }
