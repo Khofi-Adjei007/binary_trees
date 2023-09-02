9a28196d6bb919c68aa557aaa6c27a7e9ebfaff1
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-09-01 18:35:49 -0700

    Twist 5

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,18 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - Checks if a node is a leaf.
- *
- * @node: Pointer to the node to check.
- *
- * Return: 1 if the node is a leaf, 0 otherwise.
- */
+* binary_tree_is_leaf - Checks if a node is a leaf.
+*
+* @node: Pointer to the node to check.
+*
+* Return: 1 if the node is a leaf, 0 otherwise.
+*/
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    /* Check if the node is NULL */
-    if (node == NULL)
-        return (0);
+/* Check if the node is NULL */
+if (node == NULL)
+return (0);
 
-    /* Check if both left and right children are NULL, indicating a leaf */
-    return (node->left == NULL && node->right == NULL);
+/* Check if both left and right children are NULL, indicating a leaf */
+return (node->left == NULL && node->right == NULL);
 }
