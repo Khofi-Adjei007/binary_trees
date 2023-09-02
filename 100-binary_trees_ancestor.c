0e91da78e79b46c594fd63279bff2f73278dea63
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-09-01 17:59:44 -0700

    Show Down

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,28 +1,31 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
- *
- * @first: Pointer to the first node.
- * @second: Pointer to the second node.
- *
- * Return: Pointer to the lowest common ancestor node.
- * If no common ancestor was found, return NULL.
- */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+* binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
+*
+* @first: Pointer to the first node.
+* @second: Pointer to the second node.
+*
+* Return: Pointer to the lowest common ancestor node.
+* If no common ancestor was found, return NULL.
+*/
+binary_tree_t *binary_trees_ancestor(
+		const binary_tree_t *first,
+		const binary_tree_t *second
+)
 {
-    if (first == NULL || second == NULL)
-        return NULL;
+if (first == NULL || second == NULL)
+return (NULL);
 
-    if (first == second)
-        return (binary_tree_t *)first;
+if (first == second)
+return ((binary_tree_t *)first);
 
-    /* Recursively search for common ancestor in the left and right subtrees */
-    binary_tree_t *left_ancestor = binary_trees_ancestor(first, second->parent);
-    binary_tree_t *right_ancestor = binary_trees_ancestor(first->parent, second);
+/* Recursively search for common ancestor in the left and right subtrees */
+binary_tree_t *left_ancestor = binary_trees_ancestor(first, second->parent);
+binary_tree_t *right_ancestor = binary_trees_ancestor(first->parent, second);
 
-    if (left_ancestor && right_ancestor)
-        return (binary_tree_t *)second;
+if (left_ancestor && right_ancestor)
+return ((binary_tree_t *)second);
 
-    return left_ancestor ? left_ancestor : right_ancestor;
+return (left_ancestor ? left_ancestor : right_ancestor);
 }
