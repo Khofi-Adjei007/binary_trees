0d794d040f48851b382a61289cfb7a66db875485
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-09-01 19:30:48 -0700

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,32 +1,32 @@
-#include "binary_trees.h" /* Include the necessary header file */
-#include <stdlib.h>       /* Include the standard library for malloc */
+#include "binary_trees.h"
 
 /**
-* binary_tree_node - Creates a binary tree node.
+* binary_tree_insert_left - Inserts a node as the left-child of another node.
 *
-* @parent: Pointer to the parent node of the new node to create.
-* @value: The integer value to store in the new node.
+* @parent: Pointer to the node to insert the left-child in.
+* @value: The value to store in the new node.
 *
-* Description:
-* This function creates a new binary tree node with the specified parent
-* node and integer value.
-*
-* Return: A pointer to the newly created binary tree node, or NULL on failure.
+* Return: Pointer to the created node, or NULL on failure or if parent is NULL.
 */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-binary_tree_t *new_node; /* Declare a pointer to a binary tree node */
+binary_tree_t *new_node;
 
-/* Allocate memory for the new node using malloc */
-new_node = malloc(sizeof(binary_tree_t));
-if (new_node == NULL) /* Check if malloc was successful */
-return (NULL);    /* Return NULL if memory allocation fails */
+if (parent == NULL)
+return (NULL);
 
-/* Initialize the new node with the provided values */
-new_node->parent = parent;               /* Set the parent pointer */
-new_node->n = value;                     /* Set the value stored in the node */
-new_node->left = new_node->right = NULL;
-/* Initialize left and right children */
+/* Create a new node */
+new_node = binary_tree_node(parent, value);
+if (new_node == NULL)
+return (NULL);
 
-return (new_node); /* Return a pointer to the newly created node */
+/* If parent already has a left-child, update links */
+if (parent->left != NULL)
+{
+new_node->left = parent->left;
+new_node->left->parent = new_node;
+}
+
+parent->left = new_node;
+return (new_node);
 }
