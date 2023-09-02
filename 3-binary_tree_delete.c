0438caef697b297eba4e45ac3d2de43c49734d3b
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-09-01 18:32:18 -0700

    Twist 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_delete - Deletes an entire binary tree.
- *
- * @tree: Pointer to the root node of the tree to delete.
- */
+* binary_tree_delete - Deletes an entire binary tree.
+*
+* @tree: Pointer to the root node of the tree to delete.
+*/
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
+if (tree == NULL)
+return;
 
-    /* Delete the left subtree */
-    binary_tree_delete(tree->left);
+/* Delete the left subtree */
+binary_tree_delete(tree->left);
 
-    /* Delete the right subtree */
-    binary_tree_delete(tree->right);
+/* Delete the right subtree */
+binary_tree_delete(tree->right);
 
-    /* Free the current node */
-    free(tree);
+/* Free the current node */
+free(tree);
 }
